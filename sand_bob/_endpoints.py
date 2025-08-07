e9887cc04e3101ba96a42684e064d06adb048bdb
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-08-07 12:08:37 +0200

    added ollama, changed default models to gpt-oss

--- sand_bob/_endpoints.py
+++ sand_bob/_endpoints.py
@@ -1,4 +1,32 @@
-def prompt_scadsai_llm(message:str, model="meta-llama/Llama-3.3-70B-Instruct"):
+
+def prompt_ollama(message:str, model="gpt-oss:20b"):
+    """A prompt helper function that sends a message to ollama and returns only the text response."""
+    import openai
+    
+    if isinstance(message, str):
+        message = [{"role": "user", "content": message}]
+        
+    # setup connection to the LLM
+    client = openai.OpenAI()
+    client.base_url = "http://localhost:11434/v1"
+    client.api_key = "none"
+    response = client.chat.completions.create(
+        model=model,
+        messages=message
+    )
+
+    print("OLLAMA model: ", model)
+    
+    # extract answer
+    result = response.choices[0].message.content
+    print(result)
+    if "</thinking>" in result:
+        result = result.split("</thinking>")[1]
+    return result
+
+
+def prompt_scadsai_llm(message:str, model="openai/gpt-oss-120b"):
+#def prompt_scadsai_llm(message:str, model="meta-llama/Llama-3.3-70B-Instruct"):
 #def prompt_scadsai_llm(message:str, model="deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct"):
     """A prompt helper function that sends a message to ScaDS.AI LLM server at 
     ZIH TU Dresden and returns only the text response.
@@ -21,6 +49,7 @@ def prompt_scadsai_llm(message:str, model="meta-llama/Llama-3.3-70B-Instruct"):
     
     # extract answer
     return response.choices[0].message.content
+
 
 def prompt_openai(message:str, model="gpt-4.1-2025-04-14"):
     """A prompt helper function that sends a message to openAI
