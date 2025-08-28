b6832c181a159ae3dec70dc6aeec56b5cc9644bf
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-08-28 08:51:55 +0200

    layout

--- sand_bob/_statusdisplay.py
+++ sand_bob/_statusdisplay.py
@@ -18,10 +18,10 @@ class StatusDisplay:
         progress_bar_html = f"""
         <div style="position: relative; width: 100%; height: 20px; background-color: #f5f5f5; border-radius: 1px; margin: 0px 0px; overflow: hidden;">
             <div style="position: absolute; top: 0; left: 0; width: {percentage}%; height: 100%; background-color: #3874CC; transition: width 0.3s ease;"></div>
-            <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: flex; align-items: left; justify-content: center; z-index: 1;">
+            <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: flex; align-items: left; justify-content: left; z-index: 1;">
                 <span style="color: #000000; font-size: 12px; position: relative; z-index: 2;">{status_text}</span>
             </div>
-            <div style="position: absolute; top: 0; left: 0; width: {percentage}%; height: 100%; display: flex; align-items: left; justify-content: center; z-index: 3;">
+            <div style="position: absolute; top: 0; left: 0; width: {percentage}%; height: 100%; display: flex; align-items: left; justify-content: left; z-index: 3;">
                 <span style="color: #ffffff; font-size: 12px;">{status_text}</span>
             </div>
         </div>
